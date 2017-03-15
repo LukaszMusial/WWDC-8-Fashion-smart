962f99189942f888a5b7ae8c72012fbac5cb074a
LukaszMusial <qkm@op.pl>
2017-03-15 17:12:23 +0100

    Nav pt2
    Sprites

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
+# Require any additional compass plugins here.
 require 'compass/import-once/activate'
 require 'singularitygs'
 require 'breakpoint'
-# Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -10,6 +10,7 @@ sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 line_comments = false
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
