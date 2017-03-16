5718273fc5231bd9f8a2ac68358db72300df0fd9
LukaszMusial <qkm@op.pl>
2017-03-16 16:44:08 +0100

    Nav pt3
    hamburger menu button

--- config.rb
+++ config.rb
@@ -10,6 +10,7 @@ sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 line_comments = false
+relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
