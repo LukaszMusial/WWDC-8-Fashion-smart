819d9e5d1edec10c198d3775a7a43fff145e4e52
LukaszMusial <qkm@op.pl>
2017-08-11 16:31:18 +0200

    js smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
+function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari, ie 9
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
 
+function elementYPosition(idElemnet)	{
+	var elm = document.getElementsById(idElemnet);
+	var y = elm.offsetTop;
+	var node = elm;
+	while(node.offsetParent && node.offsetParent =! document.body)	{
+		node = node.offsetParent
+		y = y + node.offsetTop;
+	}
+	return y;
+}
+
+function smoothScroll (idElemnet)	{
+	var startY = currentYPosition();
+	var stopY = elementYPosition(idElemnet);
+	var distance = stopY > startY ? stopY - startY : startY - stopY;
+	var speed = math.round(distance / 100);
+	var step = math.round(distance / 25);
+	var leap = stopY > startY ? startY + step : startY - step;
+	var time = 0;
+
+	if(stopY > startY)
+		for(i=startY; i<stopY; i+=step)	{
+			setTimeOut(scrollTo(0, +'leap'+), time * speed);
+			leap += step;
+			if(leap>stopY) leap=stopY;
+			time++;
+		}
+	}
+
+	else
+		for(i=startY; i>stopY; i-=step)	{
+			setTimeOut(scrollTo(0, +'leap'+), time * speed);
+			leap -= step;
+			if(leap<stopY) leap=stopY;
+			time++;
+		}
+	}
+}
